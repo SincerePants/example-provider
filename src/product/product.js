aeddbf5312fe7801e9d88c1414336baf50ed896a
Arpan <agarwalarpan@gmail.com>
2023-11-06 19:34:19 +0530

    Non-contract-breaking-change- Add price field

--- src/product/product.js
+++ src/product/product.js
@@ -1,9 +1,9 @@
 class Product {
     constructor(id, type, name, version) {
-
         this.id = id;
         this.type = type;
         this.name = name;
+        this.price = 30.0;
         this.version = version;
     }
 }
